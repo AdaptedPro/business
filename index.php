219c5777ec14ab39e905817cdc8d04071e81da4a
AJ <info@adaptedpro.net>
2013-10-23 17:31:04 -0700

    Adding framework
    Building jswing framework

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Experiments in Code</title>
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic&v1" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="css/master.css" />
+        <!-- 
+        <link rel="stylesheet" type="text/css" href="http://soulwire.co.uk/css/style.css" />
+         -->
+        <style type="text/css">
+            @-webkit-keyframes loading { 0% { background-position: 0 0; } 100% { background-position: 16px 0; } }
+            @-moz-keyframes loading { 0% { background-position: 0 0; } 100% { background-position: 16px 0; } }
+        </style>
+        <meta name="author" content="Adam James">
+        <meta name="copyright" content="&copy; 2004 - <?php echo date('Y')?> AdaptedPro">
+        <meta name="description" content="Experiments in Code">
+        <script src="js/lib/modernizr.js"></script>     
+    </head>
+    <body>
+    <div id="sections">
+            <section id="nodes" data-slug="nodes" data-controller="AdaptedPro.Nodes">
+                <ul class="hub social">
+                    <li class="twitter"><a href="" target="_blank"><span>Twitter</span></a></li>
+                    <li class="linkedin"><a href="" target="_blank"><span>Linked in</span></a></li>
+                    <li class="plus"><a href="" target="_blank"><span>Google +</span></a></li>
+                    <li class="blog"><a href="" target="_blank"><span>My Blog</span></a></li>
+                    <li class="vimeo"><a href="" target="_blank"><span>Vimeo</span></a></li>
+                    <li class="email"><a href="" target="_blank"><span>Email</span></a></li>
+                    <li class="flickr"><a href="" target="_blank"><span>Flickr</span></a></li>
+                    <li class="jsdoit"><a href="" target="_blank"><span>jsdo.it</span></a></li>
+                    <li class="dspn"><a href="" target="_blank"><span>DSPN</span></a></li>
+                    <li class="visualize"><a href="" target="_blank"><span>visualize</span></a></li>
+                    <li class="github"><a href="" target="_blank"><span>Github</span></a></li>
+                </ul>
+            </section>
+	</div> 
+	<script src="https://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>	
+	<script type="text/javascript" src="js/scripts.min.js"></script>	           
+	</body>
+</html>
